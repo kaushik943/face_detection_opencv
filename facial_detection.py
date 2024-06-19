5d3e02cc01d7f909591858bdc09cfe41433190fe
kaushik943 <88033660+kaushik943@users.noreply.github.com>
2024-06-19 06:59:34 -0700

    Update facial_detection.py

--- facial_detection.py
+++ facial_detection.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 # video capture
-cap = cv2.VideoCapture("mi2.mp4")
+cap = cv2.VideoCapture(0)
 # starting a loop
 while 1:
     # reads frames from a camera
